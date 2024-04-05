c7ea1e586c0bab485b6b63500ff21ab302e0d332
chenyuxin <c@>
2024-04-05 17:37:17 +0800

    feat: chrome extension支持配置请求地址

--- chrome-extension/js/bg.js
+++ chrome-extension/js/bg.js
@@ -1,4 +1,3 @@
-
 chrome.runtime.onInstalled.addListener(function () {
     chrome.contextMenus.create({
         id: 'sendUrlMenu',
@@ -6,11 +5,16 @@ chrome.runtime.onInstalled.addListener(function () {
         type: 'normal',
         contexts: ['page'],
     });
+});
 
-    chrome.contextMenus.onClicked.addListener(function(info, tab) {
-        if (info.menuItemId === 'sendUrlMenu') {
-            const url = tab.url;
-            fetch('http://localhost:8082/bookmark/addTask', {
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === 'sendUrlMenu') {
+        const url = tab.url;
+        // 在这里获取 requestUrl
+        chrome.storage.sync.get('requestUrl', (data) => {
+            let requestUrl = data.requestUrl || "http://localhost:3005"; // 如果没有获取到值，使用默认地址
+
+            fetch(`${requestUrl}/bookmark/addTask`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -19,18 +23,19 @@ chrome.runtime.onInstalled.addListener(function () {
                     url: url
                 }),
             })
-            .then(text => {
-                console.log('请求成功:', text);
-                // 如果接口返回的是字符串 "OK"，将菜单项设置为不可点击
-                // if (text === 'OK') {
-                    // chrome.contextMenus.update('sendUrlMenu', {
-                        // enabled: false
-                    // });
-                // }
-            })
-            .catch((error) => {
-                console.error('请求失败:', error);
-            });
-        }
-    })
+                .then(response => response.text())
+                .then(text => {
+                    console.log('请求成功:', text);
+                    // 如果接口返回的是字符串 "OK"，将菜单项设置为不可点击
+                    // if (text === 'OK') {
+                    //     chrome.contextMenus.update('sendUrlMenu', {
+                    //         enabled: false
+                    //     });
+                    // }
+                })
+                .catch((error) => {
+                    console.error('请求失败:', error);
+                });
+        });
+    }
 });
--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,41 +1,52 @@
-let currUrl;
-chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-    currUrl = tabs[0].url;
+chrome.storage.sync.get('requestUrl', (data) => {
+    let requestUrl = data.requestUrl || "http://localhost:3005"; // 如果没有获取到值，使用默认地址
 
-    fetch('http://localhost:8082/bookmark/checkTask?url=' + encodeURIComponent(currUrl))
-        .then(response => response.text())  // 将响应转换为文本
-        .then(data => {
-            let sendBtn = document.getElementById('send-btn');
-            if (data === 'EXIST TASK' || data === 'EXIST INDEX') {
-                sendBtn.innerText = '当前网页' + data;
-                sendBtn.disabled = true;
-                sendBtn.style = ''; // 清空按钮样式
-                sendBtn.style.backgroundColor = 'gray';
-            }
-        })
-        .catch((error) => {
-            console.error('请求失败:', error);
-        });
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        let currUrl = tabs[0].url;
+        fetch(`${requestUrl}/bookmark/checkTask?url=` + encodeURIComponent(currUrl))
+            .then(response => response.text())  // 将响应转换为文本
+            .then(data => {
+                let sendBtn = document.getElementById('send-btn');
+                if (data === 'EXIST TASK' || data === 'EXIST INDEX') {
+                    sendBtn.innerText = '当前网页' + data;
+                    sendBtn.disabled = true;
+                    sendBtn.style = ''; // 清空按钮样式
+                    sendBtn.style.backgroundColor = 'gray';
+                }
+            })
+            .catch((error) => {
+                console.error('请求失败:', error);
+            });
+    });
 });
-
 
 document.getElementById('send-btn').addEventListener('click', function () {
     var jsPath = document.getElementById('html-js-path').value;
-    fetch('http://localhost:8082/bookmark/addTask', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            url: currUrl,
-            jsPath: jsPath
-        }),
-    }).then(response => response.text())
-        .then(text => {
-            console.log('请求成功:', text);
-            window.close();
-        })
-        .catch((error) => {
-            console.error('请求失败:', error);
+
+    // 因为是在事件监听器中，需要再次从存储中获取 requestUrl
+    chrome.storage.sync.get('requestUrl', (data) => {
+        let requestUrl = data.requestUrl || "http://localhost:3005"; // 如果没有获取到值，使用默认地址
+
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            let currUrl = tabs[0].url;
+
+            fetch(`${requestUrl}/bookmark/addTask`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    url: currUrl,
+                    jsPath: jsPath
+                }),
+            }).then(response => response.text())
+                .then(text => {
+                    console.log('请求成功:', text);
+                    window.close();
+                })
+                .catch((error) => {
+                    console.error('请求失败:', error);
+                });
         });
-})
+    });
+});
