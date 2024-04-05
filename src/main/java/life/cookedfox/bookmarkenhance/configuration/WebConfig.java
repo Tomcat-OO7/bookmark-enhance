ccaae69c1e15de09440e463ae16dd8117ea9aca8
chenyuxin <c@>
2024-04-05 16:56:45 +0800

    feat: springboot统一维护端点及静态资源

--- src/main/java/life/cookedfox/bookmarkenhance/configuration/WebConfig.java
+++ src/main/java/life/cookedfox/bookmarkenhance/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package life.cookedfox.bookmarkenhance.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,6 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/snapshot/**")
+                .addResourceLocations("file:./snapshot/");
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
 
